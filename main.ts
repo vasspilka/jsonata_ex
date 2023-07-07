1ff288b47aab4b4e3fb44b3011a7a4a3db9663a4
Vasilis Spilka <vasspilka@gmail.com>
2023-07-08 01:11:18 +0200

    simple jsonata server

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
-import { Server } from "websi";
-import { GET } from "websi/route";
-import * as Response from "websi/response";
+import http, { IncomingMessage, ServerResponse } from "http";
+import jsonata from "jsonata";
 
-const routes = [GET("/", () => Response.OK("Hello, Websi!"))];
 
-const server = Server(routes);
-export default server;
+const host = 'localhost';
+const port = 8080;
+
+const requestListener = function (req: IncomingMessage, res: ServerResponse) {
+
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+    req.on('end', () => {
+
+        let data = JSON.parse(body)
+
+        const expression = jsonata(data.template);
+        expression.evaluate(data.data).then(result => {
+            res.setHeader("Content-Type", "application/json");
+            res.writeHead(200);
+            res.end(JSON.stringify(result));
+        })
+
+    });
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
